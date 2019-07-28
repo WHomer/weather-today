88a2f5b026f90657ae5b8f59bd86c60b05861777
William Homer <billyhomeriii@gmail.com>
2019-07-27 20:18:18 -0600

    add forecast controller with show and test

---
+++ app/controllers/api/v1/forecast_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::ForecastController < ApplicationController
+  def show
+    render json: forecast_data(params[:location])
+  end
+
+  private
+
+  def forecast_data(location)
+    geolocation = GoogleGeolocationService.new.get_geocode(location)
+    lat_lng_string = geolocation[:results][0][:geometry][:location].values.join(',')
+    forecast = DarkskyService.new.get_forecast(lat_lng_string)
+
+    forecast_serializer = ForecastSerializer.new(geolocation, forecast)
+    forecast_serializer.get_forecast
+  end
+end
