6832ad22c7ac3bf2dddad9778c518c4dbf0ff957
William Homer <billyhomeriii@gmail.com>
2019-07-28 11:53:58 -0600

    add background controller and request spec

---
+++ app/controllers/api/v1/background_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::BackgroundController < ApplicationController
+  def index
+    render json: background_images_by_city(params['location'])
+  end
+
+  private
+
+  def background_images_by_city(location)
+    flickr_service = FlickrService.new()
+    image_results = flickr_service.get_city_backgrounds(location)
+    results = serialize_images(image_results[:photos][:photo])
+    { data: results }.to_json
+  end
+
+  def serialize_images(images)
+    images.map do |image|
+      ImageUrlSerializer.new(image).return_json
+    end
+  end
+
+end
