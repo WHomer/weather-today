f27191dc413eab61a9cac185e165e5dd4b43dc22
William Homer <billyhomeriii@gmail.com>
2019-07-29 10:17:17 -0600

    wip, add yelp resonse to munchies controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -2,11 +2,14 @@ class Api::V1::MunchiesController < ApplicationController
   def index
     start_location = params[:start]
     end_location = params[:end]
+    category = params[:food]
     google = GoogleService.new()
     response = google.get_travel_time(start_location, end_location)
     time_in_seconds = response[:routes][0][:legs][0][:duration][:value]
 
-    
+    yelp = YelpService.new()
+    arrival_time = (Time.now + time_in_seconds.seconds).to_i 
+    response = yelp.get_restaurants(end_location, arrival_time, category)
     require 'pry'; binding.pry
     render json: {}
   end
